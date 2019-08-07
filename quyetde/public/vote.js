c954b4472e93963a7efd9af5426febc570b5bee6
lam268 <lamlevu26@gmail.com>
2019-08-07 21:21:23 +0700

    done

--- quyetde/public/vote.js
+++ quyetde/public/vote.js
@@ -1,63 +1,60 @@
 window.onload = () => {
-    fetch(`/vote`,{
-        method: 'GET',
-    })
-    .then((response)=>{
-        return response.json();
-    })
-    .then((data)=>{
-        if(data.success)
-        {
-            var reciveData = JSON.parse(data.data);
-            console.log(reciveData);
-            let questionContent = document.getElementById("question-content");
-            var questionId = reciveData.questionId;
-            let like = document.getElementById("like");
-            let dislike = document.getElementById("dislike");
-            questionContent.innerText = reciveData.questionContent;
-            like.addEventListener("click",(event)=>{
-                fetch(`/vote/${questionId}/like`,{
-                    method: 'PUT',
-                })
-                .then((response)=>{
-                    return response.json();
-                })
-                .then((data)=>{
-                    window.location.href = `/questions/${questionId}`;
-                })
-                .catch((error)=>{
-                    console.log(error);
-                    window.alert(error.message);
+    fetch(`/vote`, {
+            method: 'GET',
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            if (data.success) {
+                var reciveData = JSON.parse(data.data);
+                console.log(reciveData);
+                let questionContent = document.getElementById("question-content");
+                var questionId = reciveData.questionId;
+                let like = document.getElementById("like");
+                let dislike = document.getElementById("dislike");
+                questionContent.innerText = reciveData.questionContent;
+                like.addEventListener("click", (event) => {
+                    fetch(`/vote/${questionId}/like`, {
+                            method: 'PUT',
+                        })
+                        .then((response) => {
+                            return response.json();
+                        })
+                        .then((data) => {
+                            window.location.href = `/questions/${questionId}`;
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            window.alert(error.message);
+                        });
                 });
-            });
-            dislike.addEventListener("click",(event)=>{
-                fetch(`/vote/${questionId}/dislike`,{
-                    method: 'PUT',
-                })
-                .then((response)=>{
-                    return response.json();
-                })
-                .then((data)=>{
-                    window.location.href = `/questions/${questionId}`;
-                })
-                .catch((error)=>{
-                    console.log(error);
-                    window.alert(error.message);
+                dislike.addEventListener("click", (event) => {
+                    fetch(`/vote/${questionId}/dislike`, {
+                            method: 'PUT',
+                        })
+                        .then((response) => {
+                            return response.json();
+                        })
+                        .then((data) => {
+                            window.location.href = `/questions/${questionId}`;
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                            window.alert(error.message);
+                        });
                 });
-            });
-            let voteResult = document.getElementById("vote-result");
-            voteResult.addEventListener("click",(event)=>{
-                window.location.href = `/questions/${questionId}`;
-            });
+                let voteResult = document.getElementById("vote-result");
+                voteResult.addEventListener("click", (event) => {
+                    window.location.href = `/questions/${questionId}`;
+                });
 
-        }
-        else
-        {  
-            window.alert("Do not exist");
-        }
-    })
-    .catch((error)=>{
-        console.log(error);
-        window.alert(error.message);
-    });
+            } else {
+                window.alert("Do not exist");
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            window.alert(error.message);
+        });
 }
--- quyetde/server.js
+++ quyetde/server.js
@@ -7,66 +7,63 @@ const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./public/vote.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public/vote.html'));
 });
 
-app.get('/vote', (req,res)=>{
-    fs.readFile('data.json',{encoding: 'utf8'},(error,data)=>{
-        if(error){
+app.get('/vote', (req, res) => {
+    fs.readFile('data.json', { encoding: 'utf8' }, (error, data) => {
+        if (error) {
             res.status(500).json({
                 success: false,
                 message: error.message,
             });
-        }
-        else {
+        } else {
             const questions = JSON.parse(data);
-            const randomIndex = Math.floor(Math.random()*questions.length);
+            const randomIndex = Math.floor(Math.random() * questions.length);
             const question = questions[randomIndex];
             res.status(201).json({
                 success: true,
                 data: JSON.stringify({
-                questionContent: question.content,
-                like: question.like,
-                dislike: question.dislike,
-                questionId: question.id,
+                    questionContent: question.content,
+                    like: question.like,
+                    dislike: question.dislike,
+                    questionId: question.id,
                 }),
             });
         }
     });
 });
 
-app.get('/ask', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./public/ask.html'));
+app.get('/ask', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public/ask.html'));
 });
 
-app.post('/create-question',(req,res)=>{
+app.post('/create-question', (req, res) => {
     const newQuestion = {
-        content:  req.body.questionContent,
+        content: req.body.questionContent,
         like: 0,
         dislike: 0,
         id: new Date().getTime(),
     };
-    
-    fs.readFile('data.json',{encoding: 'utf8'},(error,data)=>{
-        if(error){
+
+    fs.readFile('data.json', { encoding: 'utf8' }, (error, data) => {
+        if (error) {
             res.status(500).json({
                 success: false,
                 message: error.message,
             });
-        }
-        else {
+        } else {
             const questions = JSON.parse(data);
             questions.push(newQuestion);
 
             fs.writeFile('data.json', JSON.stringify(questions), (err) => {
-                if(err){
+                if (err) {
                     res.status(500).json({
                         success: false,
                         message: err.message,
                     });
-                }
-                else {
+                } else {
                     res.status(201).json({
                         success: true,
                         data: newQuestion,
@@ -77,38 +74,34 @@ app.post('/create-question',(req,res)=>{
     });
 });
 
-app.get("/questions/:questionId", (req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./public/question-detail.html'));
+app.get("/questions/:questionId", (req, res) => {
+    res.sendFile(path.resolve(__dirname, './public/question-detail.html'));
 });
 
-app.put("/vote/:questionId/:vote", (req,res)=>{
+app.put("/vote/:questionId/:vote", (req, res) => {
     // params: thanh phan cua duong dan co the thay doi
-    fs.readFile('data.json',{encoding: 'utf8'},(error,data)=>{
-        if(error){
+    fs.readFile('data.json', { encoding: 'utf8' }, (error, data) => {
+        if (error) {
             res.status(500).json({
                 success: false,
                 message: error.message,
             });
-        }
-        else {
+        } else {
             const questionId = req.params.questionId;
             const vote = req.params.vote;
             const questions = JSON.parse(data);
             let found = 0;
-            for (let question of questions)
-            {
-                if(question.id==questionId)
-                {
+            for (let question of questions) {
+                if (question.id == questionId) {
                     found = 1;
                     question[vote]++;
                     fs.writeFile('data.json', JSON.stringify(questions), (err) => {
-                        if(err){
+                        if (err) {
                             res.status(500).json({
                                 success: false,
                                 message: err.message,
                             });
-                        }
-                        else {
+                        } else {
                             res.status(201).json({
                                 success: true,
                             });
@@ -117,8 +110,7 @@ app.put("/vote/:questionId/:vote", (req,res)=>{
                     break;
                 }
             }
-            if(found==0)
-            {
+            if (found == 0) {
                 res.status(500).json({
                     success: false,
                     message: "Question does not exist",
@@ -128,23 +120,20 @@ app.put("/vote/:questionId/:vote", (req,res)=>{
     });
 });
 
-app.get("/get-question-by-id/:questionId", (req,res)=>{
+app.get("/get-question-by-id/:questionId", (req, res) => {
     // params: thanh phan cua duong dan co the thay doi
-    fs.readFile('data.json',{encoding: 'utf8'},(error,data)=>{
-        if(error){
+    fs.readFile('data.json', { encoding: 'utf8' }, (error, data) => {
+        if (error) {
             res.status(500).json({
                 success: false,
                 message: error.message,
             });
-        }
-        else {
+        } else {
             const questionId = req.params.questionId;
             const questions = JSON.parse(data);
             let found = 0;
-            for (let question of questions)
-            {
-                if(question.id==questionId)
-                {
+            for (let question of questions) {
+                if (question.id === questionId) {
                     found = 1;
                     sendData = JSON.stringify({
                         questionContent: question.content,
@@ -158,8 +147,7 @@ app.get("/get-question-by-id/:questionId", (req,res)=>{
                     break;
                 }
             }
-            if(found==0)
-            {
+            if (found == 0) {
                 res.status(500).json({
                     success: false,
                     message: "Question does not exist",
@@ -169,12 +157,10 @@ app.get("/get-question-by-id/:questionId", (req,res)=>{
     });
 });
 
-app.listen(3000, (error)=>{
-    if(error)
-    {
+app.listen(3000, (error) => {
+    if (error) {
         console.log(error);
-    }
-    else{
+    } else {
         console.log('Server listen on port 3000...');
     }
 });
